e3dd00ade3049a95d2edddcae79299cb3c13376a
Equan <equanrehman786@gmail.com>
2020-10-14 03:57:29 -0700

    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
+
+
+const App = ()=>{
+
+  const APP_ID = "491612e9"
+  const APP_KEY = "bc90add79ed7616e3cf64c6f21b10312"
+  var query = "chicken"
+
+  const examplereq =  `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter,setcounter] = useState(0);
+
+
+  useEffect(()=>{
+    console.log("Effect used")
+  })
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form className="search-form">
+          <input type="text" className="search-bar"/>
+          <button type="submit" className="search-button" onClick={()=>setcounter(counter+1)}>
+            {counter}
+          </button>
+
+
+        </form>
     </div>
   );
 }
